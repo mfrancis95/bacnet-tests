d5ce6dce8b7b706470e2a082ff45d20a7854f294
mfrancis95 <mikefrancis95@gmail.com>
2020-01-05 21:46:02 -0500

    Add functions for printing failure, info and success messages

--- tests.sh
+++ tests.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
+failure() {
+    message 31 "$1"
+}
+
+info() {
+    message 34 "$1"
+}
+
+message() {
+    echo -e "\e[$1m$2"
+}
+
+success() {
+    message 32 "$1"
+}
+
 # Who-Is Test
-echo -e '\e[34mRunning Who-Is test...'
+info 'Running Who-Is test...'
 bacwi > /dev/null 2> temp.txt
 # Received I-Am Request from $device_id, MAC = $ip BAC0
 read received iam request from device_id mac equals ip bac0 < temp.txt
 if [ "$received" != Received ]; then
-    echo -e "\e[31mFailed Who-Is test. Can't continue with the rest of the tests because a device hasn't been found."
+    failure "Failed Who-Is test. Can't continue with the rest of the tests because a device hasn't been found."
     exit 1
 fi
 device_id=${device_id::-1}
-echo -e '\e[32mPassed Who-Is test.'
+success 'Passed Who-Is test.'
