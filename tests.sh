81867af643dbcb0ac832702bb0dd58a443237efb
mfrancis95 <mikefrancis95@gmail.com>
2020-01-05 22:08:28 -0500

    Fail the Who-Is test early if the bacwi executable isn't found

--- tests.sh
+++ tests.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 failure() {
     message 31 "$1"
 }
@@ -18,6 +16,10 @@ success() {
 
 # Who-Is Test
 info 'Running Who-Is test...'
+if ! type bacwi &> /dev/null; then
+    failure "Failed Who-Is test. bacwi executable wasn't found."
+    exit 1
+fi
 bacwi > /dev/null 2> temp.txt
 # Received I-Am Request from $device_id, MAC = $ip BAC0
 read received iam request from device_id mac equals ip bac0 < temp.txt
