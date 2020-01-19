73bf3b788da993138a8af95182de54b471232f74
mfrancis95 <mikefrancis95@gmail.com>
2020-01-19 11:42:59 -0500

    Add Device Property Read tests and require $EXPECTED environment variable

--- tests.sh
+++ tests.sh
@@ -1,3 +1,13 @@
+DEVICE_OBJECT_TYPE=8
+
+compare() {
+    if [ "$2" == "$3" ]; then
+        success "Passed $1 test. Expected value: $2."
+    else
+        failure "Failed $1 test. Returned value $3 does not match expected value $2."
+    fi
+}
+
 failure() {
     message 31 "$1"
 }
@@ -8,6 +18,13 @@ info() {
 
 message() {
     echo -e "\e[$1m$2"
+}
+
+read_property() {
+    grep $2 $DEVICE_MAPPING_FILE > temp.txt
+    read property property_id < temp.txt
+    value=$(bacrp $1 $DEVICE_OBJECT_TYPE 0 $property_id)
+    value=${value::-1}
 }
 
 success() {
@@ -29,3 +46,15 @@ if [ "$received" != Received ]; then
 fi
 device_id=${device_id::-1}
 success 'Passed Who-Is test.'
+
+info 'Running Device Read Property tests...'
+IFS='='
+read_property $device_id Object_Identifier
+compare Object_Identifier '(device, 0)' "$value"
+read_property $device_id Object_Type
+compare Object_Type $DEVICE_OBJECT_TYPE $value
+while read property expected; do
+    read_property $device_id $property
+    compare $property $expected $value
+done < $DEVICE_FILE
+info 'Finished running tests.'
